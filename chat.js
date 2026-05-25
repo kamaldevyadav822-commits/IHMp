38ed42d5564395117ccd642315a1b4ad3052d78f
kamaldevyadav822-commits <kamaldevyadav822@gmail.com>
2026-05-25 10:45:35 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,17 +1,105 @@
+import {
+onAuthStateChanged,
+signOut
+}
+from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
+
+import {
+collection,
+query,
+orderBy,
+onSnapshot
+}
+from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
+
+import {
+auth,
+db
+}
+from './firebase-config.js';
+
+const username =
+document.getElementById('username');
+
+const chatList =
+document.querySelector('.chat-list');
+
 const logoutBtn =
 document.getElementById('logoutBtn');
 
-window.openRoom = function(user){
+/* AUTH */
+
+onAuthStateChanged(auth,(user)=>{
+
+if(!user){
 
 window.location.href =
-`room.html?user=${user}`;
+'index.html';
 
+return;
 }
 
-logoutBtn.addEventListener('click',()=>{
+username.innerText =
+'User-' + user.uid.slice(0,5);
+
+/* LOAD ROOMS */
+
+const q = query(
+collection(db,'rooms'),
+orderBy('updatedAt','desc')
+);
+
+onSnapshot(q,(snapshot)=>{
+
+chatList.innerHTML = '';
+
+snapshot.forEach((docSnap)=>{
+
+const room = docSnap.data();
+
+const div =
+document.createElement('div');
+
+div.className = 'chat-item';
+
+div.innerHTML = `
+<img src="https://i.pravatar.cc/150?u=${docSnap.id}">
+
+<div class="chat-info">
+<h4>${room.name || 'Chat Room'}</h4>
+<p>${room.lastMessage || 'Start chatting...'}</p>
+</div>
+
+<div class="online-dot"></div>
+`;
+
+div.onclick = ()=>{
+
+window.location.href =
+\`room.html?id=\${docSnap.id}\`;
+
+};
+
+chatList.appendChild(div);
+
+});
+
+});
+
+});
+
+/* LOGOUT */
+
+logoutBtn.addEventListener(
+'click',
+async ()=>{
+
+await signOut(auth);
 
 localStorage.clear();
 
-window.location.href = 'index.html';
+window.location.href =
+'index.html';
 
-});
+}
+);
